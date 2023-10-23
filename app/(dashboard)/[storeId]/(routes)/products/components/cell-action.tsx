dca975ecd08944c965032d6746ff66288c4e4139
Emmanuel nkongolo <nkongolo64@gmail.com>
2023-10-23 23:43:07 +0200

    create product page and routes with the APIs configuration and updating the MySQL database and the data structure

--- app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -16,10 +16,10 @@ import {
 import { Button } from "@/components/ui/button"
 import { AlertModal } from "@/components/modals/alert-modal"
 
-import { BillboardColumn } from "./columns"
+import { ProductColumn } from "./columns"
 
 interface CellActionProps {
-    data: BillboardColumn
+    data: ProductColumn
 }
 
 export const CellAction: React.FC<CellActionProps> = ({
@@ -33,17 +33,17 @@ export const CellAction: React.FC<CellActionProps> = ({
     
     const onCopy = (id: string) => {
         navigator.clipboard.writeText(id)
-        toast.success("Billboard Id copied to the clipboard.")
+        toast.success("Product Id copied to the clipboard.")
     }
 
     const onDelete = async () => {
         try{
             setLoading(true)
-            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
+            await axios.delete(`/api/${params.storeId}/products/${data.id}`)
             router.refresh()
-            toast.success("Billboard deleted.")
+            toast.success("Product deleted.")
         }catch (error) {
-            toast.error("Make sure you removed all categories using this billoard first.")
+            toast.error("Something went wrong.")
         } finally {
             setLoading(false)
             setOpen(false)
@@ -73,7 +73,7 @@ export const CellAction: React.FC<CellActionProps> = ({
                         <Copy className="mr-2 h-4 w-4" />
                         Copy Id
                     </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}>
+                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/products/${data.id}`)}>
                         <Edit className="mr-2 h-4 w-4" />
                         Update
                     </DropdownMenuItem>
--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -9,13 +9,13 @@ import { Separator } from "@/components/ui/separator"
 import { DataTable } from "@/components/ui/data-table"
 import { ApiList } from "@/components/ui/api-list"
 
-import { BillboardColumn, columns } from "./columns"
+import { ProductColumn, columns } from "./columns"
 
-interface BillboardClientProps {
-    data: BillboardColumn[]
+interface ProductClientProps {
+    data: ProductColumn[]
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({
+export const ProductClient: React.FC<ProductClientProps> = ({
     data
 }) => {
     const router = useRouter()
@@ -25,21 +25,21 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
         <>
         <div className="flex items-center justify-between">
             <Heading
-            title={`Billboards (${data.length})`}
-            description="Manage billboards for your store"
+            title={`Products (${data.length})`}
+            description="Manage products for your store"
             />
-            <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+            <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
                 <Plus className="mr-2 h-4 w-4"/>
                 Add New
             </Button>
         </div>
         <Separator />
-        <DataTable searchKey="label" columns={columns} data={data} />
+        <DataTable searchKey="name" columns={columns} data={data} />
         <Heading 
-        title="API" description="API calls for Billboards"
+        title="API" description="API calls for Produts"
         />
         <Separator />
-        <ApiList entityName="billboards" entityIdName="billboardId" />
+        <ApiList entityName="products" entityIdName="productId" />
         </>
     )
 }
